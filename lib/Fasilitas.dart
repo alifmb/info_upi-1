829baf5ec6cb723f4d9d23d54654f1541326b102
alifmb <56615877+alifmb@users.noreply.github.com>
2022-05-20 17:23:55 +0700

    Update Fasilitas.dart

--- lib/Fasilitas.dart
+++ lib/Fasilitas.dart
@@ -4,8 +4,21 @@ class Fasilitas extends StatelessWidget {
   const Fasilitas({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('fasilitas!'),
+    return Center(
+      child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+        Container(
+           child: Text("Kolam Renang", 
+           textAlign: TextAlign.center, 
+           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+           )
+        ),
+        Container(
+           child: Text("Gymnasium", 
+           textAlign: TextAlign.center, 
+           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+           )
+        ),
+      ],),
     );
   }
 }
